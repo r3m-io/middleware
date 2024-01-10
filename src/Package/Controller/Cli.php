4dcfcd643e63bc088bb10c04fbdfdf631162d06c
remco van der Velde <remco.pc@outlook.com>
2024-01-10 18:44:30 +0100

    Cli (php stan)

--- src/Package/Controller/Cli.php
+++ src/Package/Controller/Cli.php
@@ -79,7 +79,7 @@ class Cli extends Controller {
             ) ||
             $module === Cli::MODULE_INFO ||
             $submodule === Cli::MODULE_INFO ||
-            $command === CLI::MODULE_INFO
+            $command === Cli::MODULE_INFO
         ){
             $subcommand = false;
         }
@@ -92,8 +92,8 @@ class Cli extends Controller {
             ) ||
             $module === Cli::MODULE_INFO ||
             $submodule === Cli::MODULE_INFO ||
-            $command === CLI::MODULE_INFO ||
-            $subcommand === CLI::MODULE_INFO
+            $command === Cli::MODULE_INFO ||
+            $subcommand === Cli::MODULE_INFO
         ){
             $action = false;
         }
@@ -106,9 +106,9 @@ class Cli extends Controller {
             ) ||
             $module === Cli::MODULE_INFO ||
             $submodule === Cli::MODULE_INFO ||
-            $command === CLI::MODULE_INFO ||
-            $subcommand === CLI::MODULE_INFO ||
-            $action === CLI::MODULE_INFO
+            $command === Cli::MODULE_INFO ||
+            $subcommand === Cli::MODULE_INFO ||
+            $action === Cli::MODULE_INFO
         ){
             $subaction = false;
         }
